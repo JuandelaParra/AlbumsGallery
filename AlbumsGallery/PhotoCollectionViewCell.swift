6b9ef7f695e38d04f35e8acb812a1763fcf74ce0
Juan Carñps de la Parra <whosju@gmail.com>
2020-11-24 12:51:50 -0600

    Storyboards and some first Views

---
+++ AlbumsGallery/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoColletionViewCell.swift
+//  AlbumsGallery
+//
+
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    
+}
